517988166f62551a093335789ea4748406b58421
Jason Collins <the.oem@gmail.com>
2017-06-04 11:26:15 -0600

    WIP on tests.

--- __mocks__/browserMock.js
+++ __mocks__/browserMock.js
@@ -1,32 +1,32 @@
-//browserMock.js
-var localStorageMock = (function() {
-    var store = {};
-
-    return {
-        getItem: function(key) {
-            return store[key] || null;
-        },
-        setItem: function(key, value) {
-            store[key] = value.toString();
-        },
-        clear: function() {
-            store = {};
-        }
-    };
-
-})();
-
-Object.defineProperty(window, 'localStorage', {
-     value: localStorageMock
-});
+// browserMock.js
+// var localStorageMock = (function() {
+//     var store = {};
+//
+//     return {
+//         getItem: function(key) {
+//             return store[key] || null;
+//         },
+//         setItem: function(key, value) {
+//             store[key] = value.toString();
+//         },
+//         clear: function() {
+//             store = {};
+//         }
+//     };
+//
+// })();
+//
+// Object.defineProperty(window, 'localStorage', {
+//      value: localStorageMock
+// });
 
 
 // ------------------------------------------------------------------------- //
 // __mocks__/localStorage.js
 // http://booksyoudontread.com/2016/10/10/mocking-window-localstorage-in-jest/
 // let localStorage = {};
-// 
-// export default {  
+//
+// export default {
 //     setItem(key, value) {
 //         return Object.assign(localStorage, {[key]: value});
 //     },
