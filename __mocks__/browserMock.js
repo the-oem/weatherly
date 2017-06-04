130fecf075139d3a5ff23688a700d1886f18043d
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-06-04 08:00:48 -0600

    Add test base and necessary mocks for tests to run

---
+++ __mocks__/browserMock.js
@@ -0,0 +1,39 @@
+//browserMock.js
+var localStorageMock = (function() {
+    var store = {};
+
+    return {
+        getItem: function(key) {
+            return store[key] || null;
+        },
+        setItem: function(key, value) {
+            store[key] = value.toString();
+        },
+        clear: function() {
+            store = {};
+        }
+    };
+
+})();
+
+Object.defineProperty(window, 'localStorage', {
+     value: localStorageMock
+});
+
+
+// ------------------------------------------------------------------------- //
+// __mocks__/localStorage.js
+// http://booksyoudontread.com/2016/10/10/mocking-window-localstorage-in-jest/
+// let localStorage = {};
+// 
+// export default {  
+//     setItem(key, value) {
+//         return Object.assign(localStorage, {[key]: value});
+//     },
+//     getItem(key) {
+//         return localStorage[key];
+//     },
+//     clear() {
+//         localStorage = {};
+//     }
+// };
---
+++ test/WeatherFull.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Header from '../lib/components/Header/Header';
+
+describe.skip('Header', () => {
+  it('should render', () => {
+
+
+
+  });
+});
