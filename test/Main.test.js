ce78559c2c2fc76216cddcc1cf88df8a98530373
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-06-06 13:05:12 -0600

    Add Main testsS

--- test/Main.test.js
+++ test/Main.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Main from '../lib/components/Main/Main';
 
-describe.only('Main', () => {
+describe('Main', () => {
   it('should be stateful', () => {
     const main = shallow(<Main />);
 
@@ -29,7 +29,42 @@ describe.only('Main', () => {
     expect(main.instance().getLocation()).toEqual('12345');
   });
 
-  it('should render', () => {
+  it('should toggle view', () => {
+    const main = shallow(<Main />);
 
+    main.instance().setState({ key: true });
+    expect(main.state('key')).toEqual(true);
+
+    main.instance().toggleView('key');
+    expect(main.state('key')).toEqual(false);
+  });
+
+  it('should render Header', () => {
+    const main = mount(<Main />);
+
+    const header = main.find('div.header');
+    expect(header).toHaveLength(1);
+  });
+
+  it('should render WeatherFull when local storage is NOT empty', () => {
+    const ls = global.localStorage;
+    ls.setItem('location', 12345);
+
+    const main = mount(<Main />);
+    expect(main.instance().getLocation()).toEqual('12345');
+
+    const weatherFull = main.find('.weather-full');
+    expect(weatherFull).toHaveLength(1);
+  });
+
+  it('should render Welcome when local storage is empty', () => {
+    const ls = global.localStorage;
+    ls.clear();
+
+    const main = mount(<Main />);
+    expect(main.instance().getLocation()).toEqual(undefined);
+
+    const welcome = main.find('.welcome-search');
+    expect(welcome).toHaveLength(1);
   });
 });
