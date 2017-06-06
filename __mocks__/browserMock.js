52d22b1ccac343bbb2f962907e90327207df5f71
Jason Collins <the.oem@gmail.com>
2017-06-06 16:18:23 -0600

    Refactor javascript and clean up some CSS. Clean up redundant local storage mocks.

--- __mocks__/browserMock.js
+++ __mocks__/browserMock.js
@@ -1,39 +1,23 @@
-// browserMock.js
-// var localStorageMock = (function() {
-//     var store = {};
-//
-//     return {
-//         getItem: function(key) {
-//             return store[key] || null;
-//         },
-//         setItem: function(key, value) {
-//             store[key] = value.toString();
-//         },
-//         clear: function() {
-//             store = {};
-//         }
-//     };
-//
-// })();
-//
-// Object.defineProperty(window, 'localStorage', {
-//      value: localStorageMock
-// });
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
 
+  clear() {
+    this.store = {};
+  }
 
-// ------------------------------------------------------------------------- //
-// __mocks__/localStorage.js
-// http://booksyoudontread.com/2016/10/10/mocking-window-localstorage-in-jest/
-// let localStorage = {};
-//
-// export default {
-//     setItem(key, value) {
-//         return Object.assign(localStorage, {[key]: value});
-//     },
-//     getItem(key) {
-//         return localStorage[key];
-//     },
-//     clear() {
-//         localStorage = {};
-//     }
-// };
+  getItem(key) {
+    return this.store[key];
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock;
